e273d3a2fdcd025725b1d6afbd14077b36a202b3
vikas_dev.java <69629963+vikasgajbhiye29@users.noreply.github.com>
2024-05-11 10:47:55 +0530

    create offer.js page successfully

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -4,69 +4,70 @@ import pic1 from './images/imgforoffer.jpeg'
 function Offer() {
     
   return (
-        <div class="container my-3">
-		<div class="container text-center">
-			<h1>Best Offers</h1>
-			<div class="row align-items-center my-3">
-				<div class="col">
-					<div class="card" style={{width: "18rem"}}>
-						<img src={pic1} class="card-img-top" alt="network error"/>
-						<div class="card-body">
-							<h5 class="card-title">One Month</h5>
-							<h3 class="card-text"><del>1000 <span>RS</span></del></h3>
-                            <p class="card-text text-success">Discount : <span>40% OFF</span></p>
-                            <p class="card-text">Offer Price : 600 RS</p>
-							<input id="user_id" type="hidden" name="userid"value="<%=user1.getUid()%>"/> 
+    <div className="container d-flex">
+			
+				<div className="card" style={{width: "18rem", padding:"1rem", margin:"1rem",  borderTop:"2px solid green", borderBottom:"2px solid green"}}>
+					<div className="card-header text-center" style={{borderTop:"2px solid black"}}><strong>Offer for 1-Month</strong></div>
+  					<img src={pic1} className="card-img-top" alt="network error" />
+  					<div className="card-body" style={{borderBottom:"2px solid black"}}>
+						<div className='text-center'>
+							<h6 className="card-text"><del>1000 <span>RS</span></del></h6>
+							<p className="card-text">Price : 600 RS</p>
+                            <p className="card-text text-success"> <span>40% OFF</span></p>
+							{/* <input id="user_id" type="hidden" name="userid"value="<%=user1.getUid()%>"/> 
                             <input id="payment_600" type="hidden" name="amount" value="600"/>
-							<button type="button" onClick="paymentStart_600()" class="btn btn-warning">Pay Now</button>
-                        </div>
-					</div>
+							<button type="button" onClick="paymentStart_600()" className="btn btn-warning">Pay Now</button> */}    
+						</div>		
+  					</div>
 				</div>
-				<div class="col">
-					<div class="card" style={{width: "18rem"}}>
-						<img src={pic1} class="card-img-top" alt="network error"/>
-						<div class="card-body">
-							<h5 class="card-title">Three Month</h5>
-							<h3 class="card-text"><del>3000 <span>RS</span></del></h3>
-                            <p class="card-text text-success"> Discount : <span>50% OFF</span></p>
-                            <p class="card-text">Offer Price : 1500 RS</p>
-							<input id="user_id" type="hidden" name="userid" value=""/> 
-                            <input id="payment_1500" type="hidden" name="amount" value="1500"/>
-							<button type="button" onClick="paymentStart_1500()" class="btn btn-warning">Pay Now</button>
-                        </div>
-					</div>
+			
+				<div className="card" style={{width: "18rem", padding:"1rem", margin:"1rem", borderTop:"2px solid green", borderBottom:"2px solid green"}}>
+					<div className="card-header text-center" style={{borderTop:"2px solid black"}}><strong>Offer for 3-Month</strong></div>
+  					<img src={pic1} className="card-img-top" alt="network error" />
+  					<div className="card-body" style={{borderBottom:"2px solid black"}}>
+						<div className=' text-center' >
+							<h6 className="card-text"><del>3000 <span>RS</span></del></h6>
+                            <p className="card-text">Price : 1500 RS</p>
+                            <p className="card-text text-success"> <span>50% OFF</span></p>
+							{/* <input id="user_id" type="hidden" name="userid"value="<%=user1.getUid()%>"/> 
+                            <input id="payment_600" type="hidden" name="amount" value="600"/>
+							<button type="button" onClick="paymentStart_600()" className="btn btn-warning">Pay Now</button> */}
+						</div>		
+  					</div>
 				</div>
-				<div class="col">
-					<div class="card" style={{width: "18rem"}}>
-						<img src={pic1} class="card-img-top" alt="network error"/>
-						<div class="card-body">
-							<h5 class="card-title">Six Month</h5>
-							<h3 class="card-text"><del>6000 <span>RS</span></del></h3>
-                            <p class="card-text text-success">Discount : <span>60% OFF</span></p>
-                            <p class="card-text">Offer Price : 2400 RS</p>
-							<input id="user_id" type="hidden" name="userid"value="<%=user1.getUid()%>"/> 
-                            <input id="payment_2400" type="hidden" name="amount" value="2400"/>
-							<button type="button" onClick="paymentStart_2400()" class="btn btn-warning">Pay Now</button>
-						</div>
-					</div>
+			
+			
+				<div className="card" style={{width: "18rem", padding:"1rem", margin:"1rem",borderTop:"2px solid green", borderBottom:"2px solid green"}}>
+					<div className="card-header text-center"style={{borderTop:"2px solid black"}}><strong>Offer for 6-Month</strong></div>
+  					<img src={pic1} className="card-img-top" alt="network error" />
+  					<div className="card-body" style={{borderBottom:"2px solid black"}}>
+					  		<div className=' text-center' >
+								<h6 className="card-text"><del>6000 <span>RS</span></del></h6>
+                            	<p className="card-text">Price : 2400 RS</p>
+                            	<p className="card-text text-success"> <span>60% OFF</span></p>
+								{/* <input id="user_id" type="hidden" name="userid"value="<%=user1.getUid()%>"/> 
+                            	<input id="payment_600" type="hidden" name="amount" value="600"/>
+								<button type="button" onClick="paymentStart_600()" className="btn btn-warning">Pay Now</button> */}
+							</div>	
+  					</div>
 				</div>
-				<div class="col">
-					<div class="card" style={{width: "18rem"}}>
-						<img src={pic1} class="card-img-top" alt="network error"/>
-						<div class="card-body">
-							<h5 class="card-title">One Year</h5>
-							<h3 class="card-text"><del>12000 <span>RS</span></del></h3>
-							<p class="card-text text-success">Discount : <span>65% OFF</span></p>
-                            <p class="card-text">Offer Price : 5000 RS</p>
-							<input id="user_id" type="hidden" name="userid" value="<%=user1.getUid()%>"/> 
-                            <input id="payment_5000"type="hidden" name="amount" value="5000"/>
-							<button type="button" onClick="paymentStart_5000()" class="btn btn-warning">Pay Now</button>
-						</div>
-					</div>
+			
+				<div className="card" style={{width: "18rem", padding:"1rem", margin:"1rem", borderTop:"2px solid green", borderBottom:"2px solid green"}}>
+					<div className="card-header text-center" style={{borderTop:"2px solid black"}}><strong>Offer for 12-Month</strong></div>
+  					<img src={pic1} className="card-img-top" alt="network error" />
+  					<div className="card-body" style={{borderBottom:"2px solid black"}}>
+					    	<div className=' text-center'>
+								<h6 className="card-text "><del>12000 <span>RS</span></del></h6>
+								<p className="card-text">Price : 5000 RS</p>
+                            	<p className="card-text text-success"> <span>65% OFF</span></p>
+								{/* <input id="user_id" type="hidden" name="userid"value="<%=user1.getUid()%>"/> 
+                            	<input id="payment_600" type="hidden" name="amount" value="600"/>
+								<button type="button" onClick="paymentStart_600()" className="btn btn-warning">Pay Now</button> */}
+							</div>	
+  					</div>
 				</div>
 			</div>
-		</div>
-	</div>
+	
   )
 }
 
